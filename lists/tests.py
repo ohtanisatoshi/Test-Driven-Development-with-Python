f0eaa280a42470cbf1c02497ddb8a8c9c59d5f73
Satoshi Ohtani <ohtanisatoshi@gmail.com>
2015-04-05 19:05:45 +0900

    add new URL to create new item

--- lists/tests.py
+++ lists/tests.py
@@ -21,26 +21,18 @@ class SomeTest(TestCase):
     expected_html = render_to_string('home.html')
     self.assertEqual(response.content.decode(), expected_html)
 
-  def test_home_page_can_save_a_POST_request(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
+class NewListTest(TestCase):
 
-    response = home_page(request)
-
+  def test_saving_a_POST_request(self):
+    self.client.post('/lists/new', data={'item_text': 'A new list item'})
     self.assertEqual(Item.objects.count(), 1)
     new_item = Item.objects.first()
     self.assertEqual(new_item.text, 'A new list item')
 
-  def test_home_page_redirects_after_POST(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
+  def test_redirects_after_POST(self):
+    response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
-    response = home_page(request)
-
-    self.assertEqual(response.status_code, 302)
-    self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
+    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
     #self.assertIn('A new list item', response.content.decode())
     #expected_html = render_to_string(
       #'home.html',
